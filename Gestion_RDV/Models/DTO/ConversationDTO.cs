ab8d26a57fb2aec17ee8f1e0de6e9dd8f96001f6
Aurel <Aurel@DESKTOP-13POGE5>
2024-06-23 12:51:35 +0200

    Coversation post de merde

--- Gestion_RDV/Models/DTO/ConversationDTO.cs
+++ Gestion_RDV/Models/DTO/ConversationDTO.cs
@@ -4,12 +4,12 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Gestion_RDV.Models.DTO
 {
-
+    //----------------------GET----------------------
     public class ConversationDTO
     {
         public int ConversationId { get; set; }
         public string? ConversationName { get; set; }
-        public List<Conversation_UserDTO>? Users { get; set; }
+        public ICollection<Conversation_UserDTO>? Users { get; set; }
     }
     public class Conversation_UserDTO
     {
@@ -18,4 +18,10 @@ namespace Gestion_RDV.Models.DTO
         public string? LastName { get; set; }
     }
 
+    //----------------------POST----------------------
+    public class ConversationPostDTO
+    {
+        public string? ConversationName { get; set; }
+        public ICollection<int>? UserIds { get; set; }
+    }
 }
